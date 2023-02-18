1cfc67c6afd41790022010bc6900abec3115650e
mz <2323346933@qq.com>
2023-02-18 20:59:26 +0800

    BitMap.fromImage

--- MzLibPlugin/src/main/java/mz/lib/minecraft/bukkit/item/map/BitMap.java
+++ MzLibPlugin/src/main/java/mz/lib/minecraft/bukkit/item/map/BitMap.java
@@ -2,11 +2,17 @@ package mz.lib.minecraft.bukkit.item.map;
 
 import mz.lib.*;
 import mz.lib.minecraft.bukkit.MzLib;
+import mz.lib.minecraft.bukkit.entity.*;
 import mz.lib.minecraft.bukkit.wrappednms.*;
+import mz.lib.mzlang.*;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
+
+import java.awt.*;
+import java.awt.image.*;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
+import java.util.List;
 
 public interface BitMap extends MzMap
 {
@@ -64,5 +70,55 @@ public interface BitMap extends MzMap
 			for(int j=0;j<height;j++)
 				canvas.setPixel0(x+i,y+j,src.getPixel0(x+i,y+j));
 		}
+	}
+	
+	static List<BitMap> fromImage(Image image,int x,int y)
+	{
+		BufferedImage tar=new BufferedImage(x*128,y*128,BufferedImage.TYPE_INT_RGB);
+		Graphics2D g=tar.createGraphics();
+		g.drawImage(image.getScaledInstance(x*128,y*128,Image.SCALE_SMOOTH),0,0,x*128,y*128,null);
+		g.dispose();
+		List<BitMap> r=new ArrayList<>();
+		RGB[][] temp=new RGB[x*128][y*128];
+		for(int i=0;i<x*128;i++)
+			for(int j=0;j<y*128;j++)
+				temp[i][j]=new RGB(new Color(tar.getRGB(i,j)));
+		for(int j=0;j<y*128;j++)
+			for(int i=0;i<x*128;i++)
+			{
+				int n=WrappedMapPalette.matchColor(temp[i][j].toColor());
+				if(n<0)
+					n+=256;
+				RGB err=temp[i][j].subtract(new RGB(WrappedMapPalette.getColors()[n]));
+				if(i+1<x*128)
+				{
+					temp[i+1][j]=temp[i+1][j].add(err.multiply(7./16));
+					err=err.multiply(9./16);
+				}
+				if(j+1<y*128)
+				{
+					if(i>0)
+					{
+						temp[i-1][j+1]=temp[i-1][j+1].add(err.multiply(3./9));
+						err=err.multiply(6./9);
+					}
+					temp[i][j+1]=temp[i][j+1].add(err.multiply(5./6));
+					err=err.multiply(1./6);
+					if(i+1<x*128)
+						temp[i+1][j+1]=temp[i+1][j+1].add(err);
+				}
+			}
+		for(int j=0;j!=y;j++)
+			for(int i=0;i!=x;i++)
+			{
+				BitMap item=MzObject.newInstance(BitMap.class);
+				MzMapCanvas canvas=new MzMapCanvas(128,128);
+				for(int l=0;l<128;l++)
+					for(int k=0;k<128;k++)
+						canvas.setPixel0(k,l,WrappedMapPalette.matchColor(temp[i*128+k][j*128+l].toColor()));
+				item.setBits(canvas.data);
+				r.add(item);
+			}
+		return r;
 	}
 }
--- MzLibPlugin/src/main/java/mz/lib/minecraft/bukkit/mzlibcommand/LoadMapSubcommand.java
+++ MzLibPlugin/src/main/java/mz/lib/minecraft/bukkit/mzlibcommand/LoadMapSubcommand.java
@@ -44,54 +44,9 @@ public class LoadMapSubcommand extends AbsLastCommandProcessor
 				im=ImageIO.read(new URL(file));
 			else
 				im=ImageIO.read(new File(file));
-			BufferedImage tar=new BufferedImage(x*128,y*128,im.getType());
 			
-			Graphics2D g=tar.createGraphics();
-			try
-			{
-				g.drawImage(im.getScaledInstance(x*128,y*128,Image.SCALE_SMOOTH),0,0,x*128,y*128,null);
-				for(int j=0;j!=y;j++)
-					for(int i=0;i!=x;i++)
-					{
-						BufferedImage s=tar.getSubimage(i*128,j*128,128,128);
-						BitMap item=MzObject.newInstance(BitMap.class);
-						MzMapCanvas canvas=new MzMapCanvas(128,128);
-						RGB[][] temp=new RGB[128][128];
-						for(int k=0;k<128;k++)
-							for(int l=0;l<128;l++)
-								temp[k][l]=new RGB(new Color(s.getRGB(k,l)));
-						for(int l=0;l<128;l++)
-							for(int k=0;k<128;k++)
-							{
-								byte n=WrappedMapPalette.matchColor(temp[k][l].toColor());
-								canvas.setPixel0(k,l,n);
-								RGB err=temp[k][l].subtract(new RGB(WrappedMapPalette.getColors()[n<0?n+256:n]));
-								if(k+1<128)
-								{
-									temp[k+1][l]=temp[k+1][l].add(err.multiply(7./16));
-									err=err.multiply(9./16);
-								}
-								if(l+1<128)
-								{
-									if(k>0)
-									{
-										temp[k-1][l+1]=temp[k-1][l+1].add(err.multiply(3./9));
-										err=err.multiply(6./9);
-									}
-									temp[k][l+1]=temp[k][l+1].add(err.multiply(5./6));
-									err=err.multiply(1./6);
-									if(k+1<128)
-										temp[k+1][l+1]=temp[k+1][l+1].add(err);
-								}
-							}
-						item.setBits(canvas.data);
-						PlayerUtil.give(sender,item.getItemStack().getRaw());
-					}
-			}
-			finally
-			{
-				g.dispose();
-			}
+			for(BitMap i:BitMap.fromImage(im,x,y))
+				PlayerUtil.give(sender,i.getItemStack().getRaw());
 		}
 		catch(IIOException e)
 		{
