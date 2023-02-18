eb207590cc4d3e11641bb1a9b79ff0cc0b9c93d7
mz <2323346933@qq.com>
2023-02-19 00:19:40 +0800

    Update NmsChunk.java

--- MzLibPlugin/src/main/java/mz/lib/minecraft/bukkit/wrappednms/NmsChunk.java
+++ MzLibPlugin/src/main/java/mz/lib/minecraft/bukkit/wrappednms/NmsChunk.java
@@ -5,7 +5,7 @@ import mz.lib.minecraft.bukkit.wrapper.*;
 import org.bukkit.*;
 
 @WrappedBukkitClass({@VersionName(value="nms.Chunk",maxVer=17),@VersionName(value="net.minecraft.world.level.chunk.Chunk",minVer=17)})
-public interface NmsChunk
+public interface NmsChunk extends WrappedBukkitObject
 {
 	@WrappedBukkitFieldAccessor(@VersionName("bukkitChunk"))
 	Chunk getBukkitChunk();
