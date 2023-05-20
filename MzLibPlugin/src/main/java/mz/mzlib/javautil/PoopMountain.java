de864299aaf6f56f53dac56d5e7ccc58d7c375ff
mz <2323346933@qq.com>
2023-05-20 21:58:22 +0800

    Create PoopMountain.java

---
+++ MzLibPlugin/src/main/java/mz/mzlib/javautil/PoopMountain.java
@@ -0,0 +1,14 @@
+package mz.mzlib.javautil;
+
+@Deprecated
+public class PoopMountain
+{
+	@Deprecated
+	public static class Bukkit
+	{
+		public enum PoopEnum
+		{
+			A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y
+		}
+	}
+}
