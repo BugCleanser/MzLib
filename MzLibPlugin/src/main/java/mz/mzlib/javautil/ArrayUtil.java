4e8ac1d70b3ccc6f7b20b6b6257da218c86351f7
mz <2323346933@qq.com>
2023-05-23 21:56:06 +0800

    [ci skip]

--- MzLibPlugin/src/main/java/mz/mzlib/javautil/ArrayUtil.java
+++ MzLibPlugin/src/main/java/mz/mzlib/javautil/ArrayUtil.java
@@ -6,50 +6,72 @@ public class ArrayUtil
 {
 	private ArrayUtil() {}
 	
+	public static Integer[] box(int[] array)
+	{
+		return Arrays.stream(array).boxed().toArray(Integer[]::new);
+	}
+	public static Long[] box(long[] array)
+	{
+		return Arrays.stream(array).boxed().toArray(Long[]::new);
+	}
+	public static Double[] box(double[] array)
+	{
+		return Arrays.stream(array).boxed().toArray(Double[]::new);
+	}
+	public static Boolean[] box(boolean[] array)
+	{
+		Boolean[] result=new Boolean[array.length];
+		for(int i=0;i<result.length;i++)
+			result[i]=array[i];
+		return result;
+	}
+	public static Character[] box(char[] array)
+	{
+		Character[] result=new Character[array.length];
+		for(int i=0;i<result.length;i++)
+			result[i]=array[i];
+		return result;
+	}
+	public static Byte[] box(byte[] array)
+	{
+		Byte[] result=new Byte[array.length];
+		for(int i=0;i<result.length;i++)
+			result[i]=array[i];
+		return result;
+	}
+	public static Short[] box(short[] array)
+	{
+		Short[] result=new Short[array.length];
+		for(int i=0;i<result.length;i++)
+			result[i]=array[i];
+		return result;
+	}
+	public static Float[] box(float[] array)
+	{
+		Float[] result=new Float[array.length];
+		for(int i=0;i<result.length;i++)
+			result[i]=array[i];
+		return result;
+	}
 	public static Object[] box(Object array)
 	{
 		if(array instanceof int[])
-			return Arrays.stream((int[])array).boxed().toArray(Integer[]::new);
+			return box((int[])array);
 		else if(array instanceof long[])
-			return Arrays.stream((long[])array).boxed().toArray(Long[]::new);
+			return box((long[])array);
 		else if(array instanceof double[])
-			return Arrays.stream((double[])array).boxed().toArray(Double[]::new);
+			return box((double[])array);
 		else if(array instanceof boolean[])
-		{
-			Boolean[] result=new Boolean[((boolean[])array).length];
-			for(int i=0;i<result.length;i++)
-				result[i]=((boolean[])array)[i];
-			return result;
-		}
+			return box((boolean[])array);
 		else if(array instanceof char[])
-		{
-			Character[] result=new Character[((char[])array).length];
-			for(int i=0;i<result.length;i++)
-				result[i]=((char[])array)[i];
-			return result;
-		}
+			return box((char[])array);
 		else if(array instanceof byte[])
-		{
-			Byte[] result=new Byte[((byte[])array).length];
-			for(int i=0;i<result.length;i++)
-				result[i]=((byte[])array)[i];
-			return result;
-		}
+			return box((byte[])array);
 		else if(array instanceof short[])
-		{
-			Short[] result=new Short[((short[])array).length];
-			for(int i=0;i<result.length;i++)
-				result[i]=((short[])array)[i];
-			return result;
-		}
+			return box((short[])array);
 		else if(array instanceof float[])
-		{
-			Float[] result=new Float[((float[])array).length];
-			for(int i=0;i<result.length;i++)
-				result[i]=((float[])array)[i];
-			return result;
-		}
+			return box((float[])array);
 		else
-			throw new ClassCastException("It's not a primitive array: "+array);
+			throw new ClassCastException("It's not a primitive array: "+array+".");
 	}
 }
