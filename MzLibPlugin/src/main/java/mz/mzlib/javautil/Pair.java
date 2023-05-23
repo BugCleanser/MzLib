4364a6ae723d4f65dcb0e600136b08d9b7bd1098
mz <2323346933@qq.com>
2023-05-23 16:26:09 +0800

    [ci skip]

--- MzLibPlugin/src/main/java/mz/mzlib/javautil/Pair.java
+++ MzLibPlugin/src/main/java/mz/mzlib/javautil/Pair.java
@@ -1,6 +1,9 @@
 package mz.mzlib.javautil;
 
-public class Pair<First,Second> implements Comparable<Pair<First,Second>>
+import java.util.Map;
+import java.util.Objects;
+
+public class Pair<First,Second> implements Map.Entry<First,Second>,Comparable<Pair<First,Second>>
 {
 	public First first;
 	public Second second;
@@ -17,5 +20,32 @@ public class Pair<First,Second> implements Comparable<Pair<First,Second>>
 		if(resultFirst!=0)
 			return resultFirst;
 		return RuntimeUtil.<Comparable<Second>>forceCast(second).compareTo(o.second);
+	}
+	@Override
+	public First getKey()
+	{
+		return first;
+	}
+	@Override
+	public Second getValue()
+	{
+		return second;
+	}
+	@Override
+	public Second setValue(Second value)
+	{
+		Second last=second;
+		second=value;
+		return last;
+	}
+	@Override
+	public boolean equals(Object o)
+	{
+		return o instanceof Map.Entry&&Objects.equals(first,((Map.Entry<?,?>)o).getKey())&&Objects.equals(second,((Map.Entry<?,?>)o).getValue());
+	}
+	@Override
+	public int hashCode()
+	{
+		return Objects.hashCode(first)^Objects.hashCode(second);
 	}
 }
