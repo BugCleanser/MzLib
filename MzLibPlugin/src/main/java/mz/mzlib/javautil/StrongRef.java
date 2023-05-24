3fc5fcfffe3011b4e8810406cbbd943db677b273
mz <2323346933@qq.com>
2023-05-24 11:44:15 +0800

    Create StrongRef.java
    [ci skip]

---
+++ MzLibPlugin/src/main/java/mz/mzlib/javautil/StrongRef.java
@@ -0,0 +1,40 @@
+package mz.mzlib.javautil;
+
+import java.util.Objects;
+
+public class StrongRef<T> implements Ref<T>
+{
+	public T target;
+	
+	public StrongRef(T value)
+	{
+		set(value);
+	}
+	
+	@Override
+	public T get()
+	{
+		return target;
+	}
+	@Override
+	public void set(T value)
+	{
+		target=value;
+	}
+	
+	@Override
+	public boolean equals(Object obj)
+	{
+		return obj instanceof Ref&&get()==((Ref<?>)obj).get();
+	}
+	@Override
+	public int hashCode()
+	{
+		return Objects.hashCode(get());
+	}
+	@Override
+	public String toString()
+	{
+		return Objects.toString(get());
+	}
+}
