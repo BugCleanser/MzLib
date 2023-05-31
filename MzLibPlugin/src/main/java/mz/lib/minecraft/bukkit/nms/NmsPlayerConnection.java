d84f440be682c03b23774f35f295087b1a6bd8c2
mz <2323346933@qq.com>
2023-05-31 23:39:20 +0800

    Revert "Update NmsPlayerConnection.java"
    This reverts commit 7136a8e347cfe7393a282e481226bc8d5f8f02ff.

--- MzLibPlugin/src/main/java/mz/lib/minecraft/bukkit/nms/NmsPlayerConnection.java
+++ MzLibPlugin/src/main/java/mz/lib/minecraft/bukkit/nms/NmsPlayerConnection.java
@@ -10,6 +10,6 @@ public interface NmsPlayerConnection extends NmsPacketListener
 	@VersionalWrappedFieldAccessor({@VersionalName("networkManager"),@VersionalName(minVer=17, value="a",maxVer=19),@VersionalName(value="b",minVer=19)})
 	NmsNetworkManager getNetworkManager();
 	
-	@WrappedBukkitFieldAccessor({@VersionName("player"),@VersionName(minVer=17, value="b",maxVer=19),@VersionName(value="c",minVer=19,maxVer=19.4f),@VersionName(value="@0",minVer=19.4f)})
+	@VersionalWrappedFieldAccessor({@VersionalName("player"),@VersionalName(minVer=17, value="b",maxVer=19),@VersionalName(value="c",minVer=19)})
 	NmsEntityPlayer getPlayer();
 }
