c3660058220e765259fbe9cb56ca6259a74ff902
lvyitian <15988566900@qq.com>
2023-06-01 11:02:50 +0800

    Update NmsWorldServer.java

--- MzLibPlugin/src/main/java/mz/lib/minecraft/bukkit/wrappednms/NmsWorldServer.java
+++ MzLibPlugin/src/main/java/mz/lib/minecraft/bukkit/wrappednms/NmsWorldServer.java
@@ -1,10 +1,9 @@
 package mz.lib.minecraft.bukkit.wrappednms;
 
 import mz.lib.minecraft.bukkit.VersionName;
-import mz.lib.minecraft.bukkit.wrapper.WrappedBukkitClass;
-import mz.lib.minecraft.bukkit.wrapper.WrappedBukkitObject;
+import mz.lib.minecraft.bukkit.wrapper.*;
 
-@WrappedBukkitClass({@VersionName(value="nms.WorldServer",maxVer=17),@VersionName(value="net.minecraft.world.level.WorldServer",minVer=17)})
+@WrappedBukkitClass({@VersionName(value="nms.WorldServer",maxVer=17),@VersionName(value={"net.minecraft.world.level.WorldServer","net.minecraft.server.level.WorldServer"},minVer=17)})
 public interface NmsWorldServer extends WrappedBukkitObject
 {
 }
