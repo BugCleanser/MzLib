5db0518805fc14e4338b07800c4cfd343abaacd8
JiHuachan <jihuasama@gmail.com>
2024-08-08 15:25:46 +0800

    [ci skip]

--- MzLibPlugin/src/main/java/mz/mzlib/example/ExampleDelegator.java
+++ MzLibPlugin/src/main/java/mz/mzlib/example/ExampleDelegator.java
@@ -1,6 +1,5 @@
 package mz.mzlib.example;
 
-import mz.mzlib.MzLib;
 import mz.mzlib.util.delegator.*;
 
 @Deprecated
@@ -9,22 +8,33 @@ public class ExampleDelegator
 	@SuppressWarnings("all")
 	public static class Test implements Cloneable
 	{
-		private final double var=114.514;
-		private final Test thiz=this;
+		private double var=114.514;
+		private Test thiz=this;
+		
 		private Test()
 		{
 		}
+		
 		private void m()
+		{
+			System.out.println(thiz.var);
+		}
+		
+		private static void m1()
 		{
 			System.out.println("HelloWorld");
 		}
-		private static void m1()
-		{
-			System.out.println("HelloWorld1");
-		}
 	}
 	
-	@DelegatorClass(Test.class)
+	public static void main(String[] args)
+	{
+		TestDelegator.m1();
+		TestDelegator t=TestDelegator.newInstance();
+		t.m();
+		t.setVar(1919.810);
+		t.m();
+	}
+	
 	public interface TestDelegator extends Delegator
 	{
 		@DelegatorCreator
@@ -32,22 +42,13 @@ public class ExampleDelegator
 		{
 			return Delegator.create(TestDelegator.class,delegate);
 		}
+		
 		@DelegatorConstructor
 		TestDelegator staticNewInstance();
 		static TestDelegator newInstance()
 		{
 			return create(null).staticNewInstance();
 		}
-		
-		@DelegatorFieldAccessor("var")
-		double getVar();
-		@DelegatorFieldAccessor("var")
-		void setVar(double var);
-		
-		@DelegatorFieldAccessor("thiz")
-		TestDelegator getThiz();
-		@DelegatorFieldAccessor("@0")
-		TestDelegator ditto();
 		
 		@DelegatorMethod("m")
 		void m();
@@ -57,22 +58,8 @@ public class ExampleDelegator
 		{
 			create(null).staticM1();
 		}
+		@DelegatorFieldAccessor("var")
+		void setVar(double var);
 	}
 	
-	public static void main(String[] args)
-	{
-		MzLib.instance.load();
-		
-		TestDelegator t=TestDelegator.newInstance();
-		System.out.println(t.getVar());
-		t=t.getThiz();
-		t.setVar(191981.0);
-		System.out.println(t.getVar());
-		t=((AbsDelegator)t).clone().cast(TestDelegator.class);
-		t.m();
-		TestDelegator.m1();
-		
-		t=Delegator.allocateInstance(TestDelegator.class);
-		System.out.println(t.getVar());
-	}
 }
