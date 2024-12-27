5d74ae0c04a13c4fbfb999a22f4fb3c569d70994
mz <2323346933@qq.com>
2024-12-27 13:29:35 +0800

    Revert "feat(core): add AlkaidFastConfigCenter utility"
    This reverts commit 01f4e3821a0bf96325d77283efe1c7b5cb5ca399.

--- MzLibCore/build.gradle.kts
+++ MzLibCore/build.gradle.kts
@@ -9,7 +9,6 @@ tasks {
 }
 
 dependencies {
-    compileOnly("org.projectlombok:lombok:1.18.24")
     implementation("io.github.karlatemp:unsafe-accessor:1.7.0")
     implementation("net.bytebuddy:byte-buddy-agent:1.12.22")
     implementation("com.google.code.gson:gson:2.8.9")
