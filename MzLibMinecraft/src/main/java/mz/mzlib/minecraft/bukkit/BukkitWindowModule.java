b29bf8f33ffb94678b26e041d7c3e2ebd0bcf8a9
mz <2323346933@qq.com>
2025-01-02 16:36:57 +0800

    fix version

--- MzLibMinecraft/src/main/java/mz/mzlib/minecraft/bukkit/BukkitWindowModule.java
+++ MzLibMinecraft/src/main/java/mz/mzlib/minecraft/bukkit/BukkitWindowModule.java
@@ -1,14 +1,16 @@
 package mz.mzlib.minecraft.bukkit;
 
+import mz.mzlib.minecraft.VersionName;
+import mz.mzlib.minecraft.VersionRange;
 import mz.mzlib.minecraft.item.ItemStack;
 import mz.mzlib.minecraft.window.ModuleWindow;
 import mz.mzlib.minecraft.window.Window;
+import mz.mzlib.minecraft.wrapper.WrapMinecraftMethod;
 import mz.mzlib.module.MzModule;
 import mz.mzlib.util.nothing.LocalVar;
 import mz.mzlib.util.nothing.Nothing;
 import mz.mzlib.util.nothing.NothingInject;
 import mz.mzlib.util.nothing.NothingInjectType;
-import mz.mzlib.util.wrapper.WrapMethod;
 import mz.mzlib.util.wrapper.WrapSameClass;
 import mz.mzlib.util.wrapper.basic.WrapperBoolean;
 
@@ -26,11 +28,12 @@ public class BukkitWindowModule extends MzModule
     @WrapSameClass(Window.class)
     public interface NothingWindow extends Window, Nothing
     {
-        @WrapMethod("@0")
-        boolean placeInOrCheck(ItemStack itemStack, int begin, int end, boolean inverted, boolean doCheck);
+        @WrapMinecraftMethod(@VersionName(name="moveItemStackTo", begin=1700))
+        boolean placeInOrCheckV1700(ItemStack itemStack, int begin, int end, boolean inverted, boolean doCheck);
         
-        @NothingInject(wrapperMethod="placeInOrCheck", type=NothingInjectType.INSERT_BEFORE, locateMethod="")
-        default WrapperBoolean placeInOrCheckOverwrite(@LocalVar(1) ItemStack itemStack, @LocalVar(2) int begin, @LocalVar(3) int end, @LocalVar(4) boolean inverted, @LocalVar(5) boolean doCheck)
+        @VersionRange(begin=1700)
+        @NothingInject(wrapperMethod="placeInOrCheckV1700", type=NothingInjectType.INSERT_BEFORE, locateMethod="")
+        default WrapperBoolean placeInOrCheckOverwriteV1700(@LocalVar(1) ItemStack itemStack, @LocalVar(2) int begin, @LocalVar(3) int end, @LocalVar(4) boolean inverted, @LocalVar(5) boolean doCheck)
         {
             return WrapperBoolean.create(this.castTo(ModuleWindow.NothingWindow::create).placeInOrCheck(itemStack, begin, end, inverted, doCheck));
         }
