6e4efb6934d3ceeed7b630641b417d79c6e7aa21
NekokeCore <core2002@163.com>
2025-02-09 21:29:30 +0800

    refactor(gradle): update fileTree to files in MzLibDemo build script
    - Change testImplementation(fileTree("../lib")) to testImplementation(files("../lib"))
    - This modification improves clarity and aligns with Gradle best practices

--- MzLibDemo/build.gradle.kts
+++ MzLibDemo/build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
 
     testImplementation(project(":MzLibCore"))
     testImplementation(project(":MzLibMinecraft"))
-    testImplementation(fileTree("../lib"))
+    testImplementation(files("../lib"))
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
