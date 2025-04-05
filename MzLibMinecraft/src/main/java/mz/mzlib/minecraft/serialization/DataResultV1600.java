9ac32392ec9b79899233564955128fab2d3c6bd0
mz <2323346933@qq.com>
2025-04-05 20:50:11 +0800

    fix

--- MzLibMinecraft/src/main/java/mz/mzlib/minecraft/serialization/DataResultV1600.java
+++ MzLibMinecraft/src/main/java/mz/mzlib/minecraft/serialization/DataResultV1600.java
@@ -3,15 +3,14 @@ package mz.mzlib.minecraft.serialization;
 import mz.mzlib.minecraft.VersionName;
 import mz.mzlib.minecraft.VersionRange;
 import mz.mzlib.minecraft.wrapper.WrapMinecraftClass;
-import mz.mzlib.minecraft.wrapper.WrapMinecraftInnerClass;
 import mz.mzlib.minecraft.wrapper.WrapMinecraftMethod;
-import mz.mzlib.util.Option;
-import mz.mzlib.util.Result;
+import mz.mzlib.util.*;
 import mz.mzlib.util.wrapper.WrapperCreator;
 import mz.mzlib.util.wrapper.WrapperFactory;
 import mz.mzlib.util.wrapper.WrapperObject;
 
 import java.util.Optional;
+import java.util.function.Consumer;
 
 @VersionRange(begin=1600)
 @WrapMinecraftClass(@VersionName(name="com.mojang.serialization.DataResult"))
@@ -25,43 +24,24 @@ public interface DataResultV1600 extends WrapperObject
         return WrapperObject.create(DataResultV1600.class, wrapped);
     }
     
+    default Option<Object> resultOrPartial(Consumer<String> onError)
+    {
+        return Option.fromOptional(this.resultOrPartial0(onError));
+    }
     @WrapMinecraftMethod(@VersionName(name="resultOrPartial"))
-    Optional<Object> resultOrPartial0();
-    default Option<Object> resultOrPartial()
-    {
-        return Option.fromOptional(this.resultOrPartial0());
-    }
+    Optional<Object> resultOrPartial0(Consumer<String> onError);
     
-    @WrapMinecraftMethod(@VersionName(name="error"))
-    Optional<Object> toError0();
-    default Option<Error> toError()
-    {
-        return Option.fromOptional(this.toError0()).map(Error.FACTORY::create);
-    }
     default Option<String> getErrorMessage()
     {
-        return toError().map(Error::getMessage);
+        Ref<String> error = new RefStrong<>(null);
+        Option<Object> ignored = this.resultOrPartial(error::set);
+        return Option.fromNullable(error.get());
     }
     
     default Result<Option<Object>, String> toResult()
     {
         for(String msg: this.getErrorMessage())
             return Result.failure(Option.none(), msg);
-        return Result.success(this.resultOrPartial());
-    }
-    
-    @WrapMinecraftInnerClass(outer=DataResultV1600.class, name=@VersionName(name="Error"))
-    interface Error extends DataResultV1600
-    {
-        WrapperFactory<Error> FACTORY = WrapperFactory.find(Error.class);
-        @Deprecated
-        @WrapperCreator
-        static Error create(Object wrapped)
-        {
-            return WrapperObject.create(Error.class, wrapped);
-        }
-        
-        @WrapMinecraftMethod(@VersionName(name="message"))
-        String getMessage();
+        return Result.success(this.resultOrPartial(ThrowableConsumer.nothing()));
     }
 }
